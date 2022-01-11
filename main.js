a41f11133e0beabcbc2bd2db896d25e14c096418
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-01-11 15:22:15 +0300

    hello world page done

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-const {app, BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, "preload.js"),
+        },
+    });
+
+    win.loadFile("index.html");
+}
+
+app.whenReady().then(() => {
+    createWindow();
+    
+});
