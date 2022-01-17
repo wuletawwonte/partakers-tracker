550aba7ad820ebf69717ae91c235881153d19dee
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-01-17 23:48:20 +0300

    vue added

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, protocol } = require("electron");
 const path = require("path");
+const url = require("url");
 
 function createWindow() {
     const win = new BrowserWindow({
@@ -7,21 +8,47 @@ function createWindow() {
         height: 600,
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
-            webSecurity: false
+            webSecurity: false,
         },
     });
 
-    win.loadFile("index.html");
+    // win.loadFile("index.html");
+    win.loadURL(
+        url.format({
+            pathname: "index.html",
+            protocol: "file",
+            slashes: true,
+        })
+    );
+
+    win.on("closed", () => {
+        win = null;
+    });
 }
 
-app.whenReady().then(() => {
-    createWindow();
-
-    app.on("activate", () => {
-        if (BrowserWindow.getAllWindows().length === 0) {
-            createWindow();
+app.on("ready", () => {
+    protocol.interceptFileProtocol(
+        "file",
+        (request, callback) => {
+            const url =
+                request.url.substr(7); /* all urls start with 'file://' */
+            callback({ path: path.normalize(`${__dirname}/${url}`) });
+        },
+        (err) => {
+            if (err) console.error("Failed to register protocol");
         }
-    });
+    );
+    createWindow();
 });
+
+// app.whenReady().then(() => {
+//     createWindow();
+
+//     app.on("activate", () => {
+//         if (BrowserWindow.getAllWindows().length === 0) {
+//             createWindow();
+//         }
+//     });
+// });
 
 // Menu.setApplicationMenu(null);
