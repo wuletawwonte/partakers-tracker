fa03fb56641e8921d510a2297e43a7a1ce81626e
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-01-18 16:45:59 +0300

    nav menu active link hover coloring fixed

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ function createWindow() {
         width: 800,
         height: 600,
         webPreferences: {
-            preload: path.join(__dirname, "preload.js"),
-            webSecurity: false,
+            preload: path.join(__dirname, "preload.js")
+            // webSecurity: false,
         },
     });
 
