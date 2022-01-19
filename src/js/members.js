70c3034e9323baa76aaf64f376accded5a3cea2c
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-01-19 21:16:05 +0300

    layout changed to flex

--- src/js/members.js
+++ src/js/members.js
@@ -4,13 +4,29 @@ document.addEventListener("DOMContentLoaded", async () => {
     let membersList = document.getElementById("app");
     let memberNames = names
         .map((member) => {
-            return "<tr>" + member.firstname + " " + member.lastname + "</tr>";
-        })
-        .join("<br />");
+            return `<tr>
+                    <td>${member.firstname} ${member.lastname}</td>
+                    <td><button>Delete</button></td>
+                </tr>
+                `;
+        }).join("");
 
     if (memberNames.length == 0) {
         membersList.innerHTML = "No Member Registered Yet";
     } else {
-        membersList.innerHTML = memberNames;
+        let tableContent = `
+            <table class="table table-hover table-info"> 
+                <thead>
+                    <tr>
+                        <th>Names</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${memberNames}
+                </tbody>
+            </table>
+        `;
+        membersList.innerHTML = tableContent;
     }
 });
