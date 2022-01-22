8ea5e64bc3df5d75b3b60decb53cab0b8aa8e72f
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-01-22 20:40:35 +0300

    save member button prevents default

---
+++ src/js/addmember.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {    
+    const saveMemberBtn = document.getElementById("saveMember");
+    saveMemberBtn.addEventListener("click", (e)=> {
+        e.preventDefault();
+        console.log("save Member Clicked");
+    });
+
+
+});
