57b4c04e9c734092f4cc08af5f9856998225fcf2
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-01-22 21:20:56 +0300

    column gap issue fixed

--- src/js/addmember.js
+++ src/js/addmember.js
@@ -1,9 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {    
+
     const saveMemberBtn = document.getElementById("saveMember");
+
     saveMemberBtn.addEventListener("click", (e)=> {
         e.preventDefault();
-        
-        console.log("save Member Clicked");
+
+        const firstname = document.getElementById("firstname");
+        const middlename = document.getElementById("middlename");
+        const lastname = document.getElementById("lastname");
+        const phoneNumber = document.getElementById("phone");
+        const amount = document.getElementById("amount");
+
+        console.log(firstname);
     });
 
 
