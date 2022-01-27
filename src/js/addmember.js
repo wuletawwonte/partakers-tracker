95b4e351ee314405222c8252ace348a2513269aa
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-01-27 10:53:21 +0300

    member registration validation error feature added

--- src/js/addmember.js
+++ src/js/addmember.js
@@ -1,44 +1,62 @@
-document.addEventListener("DOMContentLoaded", () => {    
-
+document.addEventListener("DOMContentLoaded", () => {
     const saveMemberBtn = document.getElementById("saveMember");
 
-    saveMemberBtn.addEventListener("click", (e)=> {
+    saveMemberBtn.addEventListener("click", (e) => {
         e.preventDefault();
 
-        let firstname = document.getElementById("firstname");
+        // Input fields
+        const firstname = document.getElementById("firstname");
         const middlename = document.getElementById("middlename");
         const lastname = document.getElementById("lastname");
         const phoneNumber = document.getElementById("phone");
         const amount = document.getElementById("amount");
 
+        // Input Field Error Message Containers
+        const firstnameError = document.getElementById("firstname-error");
+        const middlenameError = document.getElementById("middlename-error");
+        const lastnameError = document.getElementById("lastname-error");
+        const phoneNumberError = document.getElementById("phone-error");
+        const amountError = document.getElementById("amount-error");
+
         const validate = () => {
-            let errorMessage = [];
-            if(firstname.validity.valueMissing) {
-                console.log(firstname.validationMessage)
-                errorMessage.push("First Name cannot be empty");
-            }
-            if(errorMessage.length == 0) {
-                return true;
+            let errorStatus = true;
+            if (firstname.validity.valueMissing) {
+                errorStatus = false;
+                firstnameError.innerHTML = "እባክዎን ስም ሳይሞሉ አይለፉ";
+                firstnameError.style = "display: block";
             } else {
-
-                return false;
+                firstnameError.style = "display: none";
             }
-        }
+            if (middlename.validity.valueMissing) {
+                errorStatus = false;
+                middlenameError.innerHTML = "እባክዎን የአባት ስም ሳይሞሉ አይለፉ";
+                middlenameError.style = "display: block";
+            } 
+            if (lastname.validity.valueMissing) {
+                errorStatus = false;
+                lastnameError.innerHTML = "እባክዎን የአያት ስም ሳይሞሉ አይለፉ";
+                lastnameError.style = "display: block";            
+            }
+            if (amount.validity.valueMissing) {
+                errorStatus = false;
+                amountError.innerHTML = "እባክዎን ወራዊ መዋጮ ሳይሞሉ አይለፉ";
+                amountError.style = "display: block";
+            }
+            return errorStatus;
+        };
 
-        if(validate()) {
+        if (validate()) {
             console.log("form properly validated");
             const newMember = {
                 firstname: firstname.value,
                 middlename: middlename.value,
                 lastname: lastname.value,
                 phoneNumber: phoneNumber.value,
-                amount: amount.value
-            }
+                amount: amount.value,
+            };
             window.api.addMember(newMember);
-            sessionStorage.setItem('message', 'የወንጌል ማህበርተኛው በትክክል ተምዝግቧል።');
-            window.location.href=`../pages/members.html`;
+            sessionStorage.setItem("message", "የወንጌል ማህበርተኛው በትክክል ተምዝግቧል።");
+            window.location.href = `../pages/members.html`;
         }
     });
-
-
 });
