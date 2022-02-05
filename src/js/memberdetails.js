833559bb205f9873ac19b0323a804d22f40d240c
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-02-05 14:51:56 +0300

    member details page menu added

---
+++ src/js/memberdetails.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const homeBtn = document.getElementById("member-details-list");
+
+    homeBtn.addEventListener("click", () => {
+        console.log("Home menu btn clicked");
+    });
+});
