193a7d2518b6993bfaebff6227b65ffaaa34672d
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-02-06 16:08:55 +0300

    member details page payment list table header added

--- src/js/memberdetails.js
+++ src/js/memberdetails.js
@@ -5,16 +5,34 @@ document.addEventListener("DOMContentLoaded", () => {
     const memberContent = document.getElementById("member-details-content");
 
     const loadHomeContent = () => {
-        aboutBtn. className = "";
+        aboutBtn.classList.remove("member-details-menu-active");
         homeBtn.className = "member-details-menu-active";
-        memberContent.innerHTML = "<h2>Home Content</h2>";
-    }
+
+        let tableContainer = `
+            <table class="table table-hover table-sm table-bordered table-striped"> 
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>ወር</th>
+                        <th>የክፍያ መጠን</th>
+                        <th>የክፍያ ሁኔታ</th>
+                        <th>ተግባር</th>
+                    </tr>
+                </thead>
+                <tbody>
+                </tbody>
+            </table>
+
+        `;
+
+        memberContent.innerHTML = `<h3>የአባል የክፍያ ሁኔታ</h3>${tableContainer}`;
+    };
 
     const loadAboutContent = () => {
-        homeBtn.className = "";
+        homeBtn.classList.remove("member-details-menu-active");
         aboutBtn.className = "member-details-menu-active";
         memberContent.innerHTML = "<h2>About Content</h2>";
-    }
+    };
 
     loadHomeContent();
 
