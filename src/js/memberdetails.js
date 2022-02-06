0792113ea29cf6c58f9a20536ec61fb3cb127ad4
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-02-06 15:36:54 +0300

    member details page menu templae works

--- src/js/memberdetails.js
+++ src/js/memberdetails.js
@@ -1,7 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     const homeBtn = document.getElementById("member-details-list");
+    const aboutBtn = document.getElementById("member-details-pay");
+
+    const memberContent = document.getElementById("member-details-content");
+
+    const loadHomeContent = () => {
+        aboutBtn. className = "";
+        homeBtn.className = "member-details-menu-active";
+        memberContent.innerHTML = "<h2>Home Content</h2>";
+    }
+
+    const loadAboutContent = () => {
+        homeBtn.className = "";
+        aboutBtn.className = "member-details-menu-active";
+        memberContent.innerHTML = "<h2>About Content</h2>";
+    }
+
+    loadHomeContent();
 
     homeBtn.addEventListener("click", () => {
-        console.log("Home menu btn clicked");
+        loadHomeContent();
+    });
+
+    aboutBtn.addEventListener("click", () => {
+        loadAboutContent();
     });
 });
